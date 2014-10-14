57d88cc6553a680b8b5fa3cada81d9cf37835438
tobiasbalzano <tobias@balzano.se>
2014-10-14 14:04:12 +0200

    la till en hemlig kommentar

---
+++ JensMemory/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace JensMemory
+{
+    partial class GameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlCardHolder = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblWhosTurn = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxInfo = new System.Windows.Forms.TextBox();
+            this.timerCompare = new System.Windows.Forms.Timer(this.components);
+            this.timerEndGame = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pnlCardHolder
+            // 
+            this.pnlCardHolder.Location = new System.Drawing.Point(12, 60);
+            this.pnlCardHolder.Name = "pnlCardHolder";
+            this.pnlCardHolder.Size = new System.Drawing.Size(400, 400);
+            this.pnlCardHolder.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(13, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 50);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Lägg till spelare";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblWhosTurn
+            // 
+            this.lblWhosTurn.AutoSize = true;
+            this.lblWhosTurn.Font = new System.Drawing.Font("Arial Narrow", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWhosTurn.Location = new System.Drawing.Point(127, 25);
+            this.lblWhosTurn.Name = "lblWhosTurn";
+            this.lblWhosTurn.Size = new System.Drawing.Size(0, 24);
+            this.lblWhosTurn.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(128, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Aktiv spelare:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(415, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Poäng:";
+            // 
+            // tbxInfo
+            // 
+            this.tbxInfo.Font = new System.Drawing.Font("Arial", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxInfo.Location = new System.Drawing.Point(418, 60);
+            this.tbxInfo.Multiline = true;
+            this.tbxInfo.Name = "tbxInfo";
+            this.tbxInfo.Size = new System.Drawing.Size(140, 400);
+            this.tbxInfo.TabIndex = 5;
+            // 
+            // timerCompare
+            // 
+            this.timerCompare.Interval = 1000;
+            this.timerCompare.Tick += new System.EventHandler(this.timerCompare_Tick);
+            // 
+            // timerEndGame
+            // 
+            this.timerEndGame.Interval = 500;
+            this.timerEndGame.Tick += new System.EventHandler(this.timerEndGame_Tick);
+            // 
+            // GameWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 462);
+            this.Controls.Add(this.tbxInfo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblWhosTurn);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnlCardHolder);
+            this.Name = "GameWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCardHolder;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblWhosTurn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxInfo;
+        private System.Windows.Forms.Timer timerCompare;
+        private System.Windows.Forms.Timer timerEndGame;
+    }
+}
+
---
+++ JensMemory/PopUp.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace JensMemory
+{
+    partial class PopUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.lblGiveName = new System.Windows.Forms.Label();
+            this.btnCreatePlayer = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(12, 63);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(206, 20);
+            this.tbxName.TabIndex = 0;
+            // 
+            // lblGiveName
+            // 
+            this.lblGiveName.AutoSize = true;
+            this.lblGiveName.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGiveName.Location = new System.Drawing.Point(12, 9);
+            this.lblGiveName.Name = "lblGiveName";
+            this.lblGiveName.Size = new System.Drawing.Size(0, 16);
+            this.lblGiveName.TabIndex = 1;
+            // 
+            // btnCreatePlayer
+            // 
+            this.btnCreatePlayer.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreatePlayer.Location = new System.Drawing.Point(12, 99);
+            this.btnCreatePlayer.Name = "btnCreatePlayer";
+            this.btnCreatePlayer.Size = new System.Drawing.Size(83, 40);
+            this.btnCreatePlayer.TabIndex = 2;
+            this.btnCreatePlayer.Text = "Skapa ny";
+            this.btnCreatePlayer.UseVisualStyleBackColor = true;
+            this.btnCreatePlayer.Click += new System.EventHandler(this.btnCreatePlayer_Click);
+            // 
+            // btnDone
+            // 
+            this.btnDone.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDone.Location = new System.Drawing.Point(153, 100);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(65, 39);
+            this.btnDone.TabIndex = 3;
+            this.btnDone.Text = "Klar";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // PopUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 151);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.btnCreatePlayer);
+            this.Controls.Add(this.lblGiveName);
+            this.Controls.Add(this.tbxName);
+            this.Name = "PopUp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Användare";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.Label lblGiveName;
+        private System.Windows.Forms.Button btnCreatePlayer;
+        private System.Windows.Forms.Button btnDone;
+    }
+}
