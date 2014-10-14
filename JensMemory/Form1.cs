72f9bdfe2aea7f4747a16e4846805170fec3264f
tobiasbalzano <tobias@balzano.se>
2014-10-14 14:25:54 +0200

    tog bort canisares och dess referenser

--- JensMemory/Form1.cs
+++ JensMemory/Form1.cs
@@ -128,12 +128,6 @@ namespace JensMemory
             }
         }
 
-        private void canisares()
-        {
-            int bajs = 5;
-            bajs = 5 + 5;
-        }
-
         private void card_Click(object sender, EventArgs e) // Metoden som skall köras när man klickar på korten
         {
             // Vi castar om object sender till Card sender så att kompilatorn känner att
@@ -168,7 +162,6 @@ namespace JensMemory
             playerTurn.RemoveAt(0);
             playerTurn.Add(activePlayer);
             activePlayer = playerTurn[0];
-            canisares();
         }
 
 
