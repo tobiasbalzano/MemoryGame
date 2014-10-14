20c681066939924f6d27f2124b9442b6de88fdf2
JensBackelin <jensbackelin@hotmail.com>
2014-10-14 14:34:14 +0200

    Ändrat size på GameWindow

--- JensMemory/Form1.Designer.cs
+++ JensMemory/Form1.Designer.cs
@@ -109,7 +109,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 462);
+            this.ClientSize = new System.Drawing.Size(1264, 682);
             this.Controls.Add(this.tbxInfo);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
