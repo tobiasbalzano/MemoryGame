3a2791c2b8663d8979f99a77906980306d9a4494
rogerertas <rogerertas@gmail.com>
2014-10-15 15:56:42 +0200

    Skapat en GUI update till ChoosePortrait. Ordningen ska nu fungera som det ska.

--- JensMemory/ChoosePortrait.cs
+++ JensMemory/ChoosePortrait.cs
@@ -17,24 +17,27 @@ namespace JensMemory
         {
             
             InitializeComponent();
+            Portrait.BackgroundImage = Portraits[0];
         }
 
         int i = 0;
         private static Image[] Portraits = { Properties.Resources.trainer1, Properties.Resources.trainer2, Properties.Resources.trainer3, Properties.Resources.trainer4, Properties.Resources.trainer5, Properties.Resources.trainer6 };
         private static Image[] Silhouettes = { Properties.Resources.trainer1S, Properties.Resources.trainer2S, Properties.Resources.trainer3S, Properties.Resources.trainer4S, Properties.Resources.trainer5S, Properties.Resources.trainer6S };
+        //public static List<Image> trainer = new List<Image>();
         int EndRange = Portraits.Count() - 1;
 
         private void LeftArrow_Click(object sender, EventArgs e)
         {
-            if (i == EndRange)
+            if (i == 0)
             {
-                i = 0;
+                i = EndRange;
             }
             else
             {
-                i++;
+                i--;
             }
-            LeftArrow.Image = Portraits[i];
+            UpdateGUI();
+
              
 
         }
@@ -46,20 +49,25 @@ namespace JensMemory
 
         private void RightArrow_Click(object sender, EventArgs e)
         {
-            if (i == 0)
+            if (i == EndRange)
             {
-                i = EndRange;
+                i = 0;
             }
             else
             {
                 i++;
             }
-            LeftArrow.Image = Portraits[i];
+            UpdateGUI();    
         }
 
         private void ChooseCharacter_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void UpdateGUI()
+        {
+            Portrait.BackgroundImage = Portraits[i];
         }
     }
 }
